c70c942fe586c05e2e43a51ad45fa07897f4e8b1
kelly <kiroegbu@gmail.com>
2025-11-23 15:06:42 +0100

    Added

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import Button from "@/components/common/Button";
 
-const About = () => {
+const About: React.FC = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold">About Page</h1>
-      <p>This page describes what this project is about.</p>
+    <div className="flex flex-col gap-4 items-center justify-center h-screen">
+      <h1 className="text-4xl font-semibold mb-6">About Page</h1>
+
+      <Button label="Small Button" size="small" shape="rounded-sm" />
+      <Button label="Medium Button" size="medium" shape="rounded-md" />
+      <Button label="Large Button" size="large" shape="rounded-full" />
     </div>
   );
 };
 
 export default About;
+
