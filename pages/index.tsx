f00519115a1b4f42cad0525c61aeeea60944d0e4
kelly <kiroegbu@gmail.com>
2025-11-23 13:54:59 +0100

    updated

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-5xl font-semibold">Welcome to My Next.js App</h1>
+    </div>
+  );
+};
+
+export default Home;
