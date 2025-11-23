cd22590e0f6090ee94aa50d32e45fb78cce39b6c
kelly <kiroegbu@gmail.com>
2025-11-23 19:20:17 +0100

    updated

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,25 +1,15 @@
 // pages/users.tsx
-import React, { useEffect, useState } from "react";
+import React from "react";
 import UserCard from "../components/common/UserCard";
 import { UserProps } from "../interfaces";
 
 
-const UsersPage = () => {
-const [users, setUsers] = useState<UserProps[]>([]);
+interface UsersPageProps {
+users: UserProps[];
+}
 
 
-useEffect(() => {
-const fetchUsers = async () => {
-const response = await fetch("https://jsonplaceholder.typicode.com/users");
-const data = await response.json();
-setUsers(data);
-};
-
-
-fetchUsers();
-}, []);
-
-
+const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
 return (
 <div className="p-6">
 <h1 className="text-2xl font-bold mb-4">Users</h1>
@@ -28,6 +18,19 @@ return (
 ))}
 </div>
 );
+};
+
+
+export const getStaticProps = async () => {
+const response = await fetch("https://jsonplaceholder.typicode.com/users");
+const users = await response.json();
+
+
+return {
+props: {
+users,
+},
+};
 };
 
 
